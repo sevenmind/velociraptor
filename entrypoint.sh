6a3f6666a68b613269a1e5fca1c68f7d8b76dae2
the1codemaster <the1codemaster@gmail.com>
2019-08-02 16:25:22 +0200

    Try to fix failure
    In this episode of trying to fix things we broke at some unknown point
    but aren't sure how, we remove trap on the off chance it is causing problems

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,7 +45,6 @@ snd|  ,     |  /\\ \\ \\__    |       \\_
                                              (o)====(o)
 EOF
 }
-trap fail EXIT
 
 # Ensure that the GitHub GPG key is present
 if [ ! "$RAPTOR_KEY" ]; then
